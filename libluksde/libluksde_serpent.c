3a75fb7c52ac39f5385a324f9972a706f18c10da
Bogdan Stefan <bogdanjsx@gmail.com>
2017-06-08 18:52:42 +0300

    Serpent ECB working

--- libluksde/libluksde_serpent.c
+++ libluksde/libluksde_serpent.c
@@ -349,46 +349,46 @@ int libluksde_serpent_context_set_key(
 
 	k += 100;
 
-	S3(r3, r4, r0, r1, r2); keysaveload(r1, r2, r4, r3, 28, 24);
-	S4(r1, r2, r4, r3, r0); keysaveload(r2, r4, r3, r0, 24, 20);
-	S5(r2, r4, r3, r0, r1); keysaveload(r1, r2, r4, r0, 20, 16);
-	S6(r1, r2, r4, r0, r3); keysaveload(r4, r3, r2, r0, 16, 12);
-	S7(r4, r3, r2, r0, r1); keysaveload(r1, r2, r0, r4, 12, 8);
-	S0(r1, r2, r0, r4, r3); keysaveload(r0, r2, r4, r1, 8, 4);
-	S1(r0, r2, r4, r1, r3); keysaveload(r3, r4, r1, r0, 4, 0);
-	S2(r3, r4, r1, r0, r2); keysaveload(r2, r4, r3, r0, 0, -4);
-	S3(r2, r4, r3, r0, r1); keysaveload(r0, r1, r4, r2, -4, -8);
-	S4(r0, r1, r4, r2, r3); keysaveload(r1, r4, r2, r3, -8, -12);
-	S5(r1, r4, r2, r3, r0); keysaveload(r0, r1, r4, r3, -12, -16);
-	S6(r0, r1, r4, r3, r2); keysaveload(r4, r2, r1, r3, -16, -20);
-	S7(r4, r2, r1, r3, r0); keysaveload(r0, r1, r3, r4, -20, -24);
-	S0(r0, r1, r3, r4, r2); keysaveload(r3, r1, r4, r0, -24, -28);
+	S3( r3, r4, r0, r1, r2 );	keysaveload( r1, r2, r4, r3, 28, 24 );
+	S4( r1, r2, r4, r3, r0 );	keysaveload( r2, r4, r3, r0, 24, 20 );
+	S5( r2, r4, r3, r0, r1 );	keysaveload( r1, r2, r4, r0, 20, 16 );
+	S6( r1, r2, r4, r0, r3 );	keysaveload( r4, r3, r2, r0, 16, 12 );
+	S7( r4, r3, r2, r0, r1 );	keysaveload( r1, r2, r0, r4, 12, 8 );
+	S0( r1, r2, r0, r4, r3 );	keysaveload( r0, r2, r4, r1, 8, 4 );
+	S1( r0, r2, r4, r1, r3 );	keysaveload( r3, r4, r1, r0, 4, 0 );
+	S2( r3, r4, r1, r0, r2 );	keysaveload( r2, r4, r3, r0, 0, -4 );
+	S3( r2, r4, r3, r0, r1 );	keysaveload( r0, r1, r4, r2, -4, -8 );
+	S4( r0, r1, r4, r2, r3 );	keysaveload( r1, r4, r2, r3, -8, -12 );
+	S5( r1, r4, r2, r3, r0 );	keysaveload( r0, r1, r4, r3, -12, -16 );
+	S6( r0, r1, r4, r3, r2 );	keysaveload( r4, r2, r1, r3, -16, -20 );
+	S7( r4, r2, r1, r3, r0 );	keysaveload( r0, r1, r3, r4, -20, -24 );
+	S0( r0, r1, r3, r4, r2 );	keysaveload( r3, r1, r4, r0, -24, -28 );
 
 	k -= 50;
 
-	S1(r3, r1, r4, r0, r2); keysaveload(r2, r4, r0, r3, 22, 18);
-	S2(r2, r4, r0, r3, r1); keysaveload(r1, r4, r2, r3, 18, 14);
-	S3(r1, r4, r2, r3, r0); keysaveload(r3, r0, r4, r1, 14, 10);
-	S4(r3, r0, r4, r1, r2); keysaveload(r0, r4, r1, r2, 10, 6);
-	S5(r0, r4, r1, r2, r3); keysaveload(r3, r0, r4, r2, 6, 2);
-	S6(r3, r0, r4, r2, r1); keysaveload(r4, r1, r0, r2, 2, -2);
-	S7(r4, r1, r0, r2, r3); keysaveload(r3, r0, r2, r4, -2, -6);
-	S0(r3, r0, r2, r4, r1); keysaveload(r2, r0, r4, r3, -6, -10);
-	S1(r2, r0, r4, r3, r1); keysaveload(r1, r4, r3, r2, -10, -14);
-	S2(r1, r4, r3, r2, r0); keysaveload(r0, r4, r1, r2, -14, -18);
-	S3(r0, r4, r1, r2, r3); keysaveload(r2, r3, r4, r0, -18, -22);
+	S1( r3, r1, r4, r0, r2 );	keysaveload( r2, r4, r0, r3, 22, 18 );
+	S2( r2, r4, r0, r3, r1 );	keysaveload( r1, r4, r2, r3, 18, 14 );
+	S3( r1, r4, r2, r3, r0 );	keysaveload( r3, r0, r4, r1, 14, 10 );
+	S4( r3, r0, r4, r1, r2 );	keysaveload( r0, r4, r1, r2, 10, 6 );
+	S5( r0, r4, r1, r2, r3 );	keysaveload( r3, r0, r4, r2, 6, 2 );
+	S6( r3, r0, r4, r2, r1 );	keysaveload( r4, r1, r0, r2, 2, -2 );
+	S7( r4, r1, r0, r2, r3 );	keysaveload( r3, r0, r2, r4, -2, -6 );
+	S0( r3, r0, r2, r4, r1 );	keysaveload( r2, r0, r4, r3, -6, -10 );
+	S1( r2, r0, r4, r3, r1 );	keysaveload( r1, r4, r3, r2, -10, -14 );
+	S2( r1, r4, r3, r2, r0 );	keysaveload( r0, r4, r1, r2, -14, -18 );
+	S3( r0, r4, r1, r2, r3 );	keysaveload( r2, r3, r4, r0, -18, -22 );
 
 	k -= 50;
 
-	S4(r2, r3, r4, r0, r1); keysaveload(r3, r4, r0, r1, 28, 24);
-	S5(r3, r4, r0, r1, r2); keysaveload(r2, r3, r4, r1, 24, 20);
-	S6(r2, r3, r4, r1, r0); keysaveload(r4, r0, r3, r1, 20, 16);
-	S7(r4, r0, r3, r1, r2); keysaveload(r2, r3, r1, r4, 16, 12);
-	S0(r2, r3, r1, r4, r0); keysaveload(r1, r3, r4, r2, 12, 8);
-	S1(r1, r3, r4, r2, r0); keysaveload(r0, r4, r2, r1, 8, 4);
-	S2(r0, r4, r2, r1, r3); keysaveload(r3, r4, r0, r1, 4, 0);
+	S4( r2, r3, r4, r0, r1 );	keysaveload( r3, r4, r0, r1, 28, 24 );
+	S5( r3, r4, r0, r1, r2 );	keysaveload( r2, r3, r4, r1, 24, 20 );
+	S6( r2, r3, r4, r1, r0 );	keysaveload( r4, r0, r3, r1, 20, 16 );
+	S7( r4, r0, r3, r1, r2 );	keysaveload( r2, r3, r1, r4, 16, 12 );
+	S0( r2, r3, r1, r4, r0 );	keysaveload( r1, r3, r4, r2, 12, 8 );
+	S1( r1, r3, r4, r2, r0 );	keysaveload( r0, r4, r2, r1, 8, 4 );
+	S2( r0, r4, r2, r1, r3 );	keysaveload( r3, r4, r0, r1, 4, 0 );
 
-	S3(r3, r4, r0, r1, r2); keysave(r1, r2, r4, r3, 0);
+	S3( r3, r4, r0, r1, r2 );	keysave( r1, r2, r4, r3, 0 );
 
 	return( 1 );
 }
@@ -461,7 +461,7 @@ int libluksde_serpent_crypt_ecb(
 			error);
 	}
 
-	return( result );
+	return ( result );
 }
 
 
@@ -472,52 +472,57 @@ int libluksde_serpent_encrypt_block(
      libcerror_error_t **error )
 {
 	uint32_t *k 					= context->key;
-	uint32_t	r0, r1, r2, r3, r4;
+	uint32_t *input 			= (uint32_t *) input_data;
+	uint32_t *output 			= (uint32_t *) output_data;
+	uint32_t	d0, d1, d2, d3, d4;
 
-	r0 = input_data[ 0 ];
-	r1 = input_data[ 1 ];
-	r2 = input_data[ 2 ];
-	r3 = input_data[ 3 ];
+	d0 = input[ 0 ];
+	d1 = input[ 1 ];
+	d2 = input[ 2 ];
+	d3 = input[ 3 ];
 
-	K(r0, r1, r2, r3, 0);
-	S0(r0, r1, r2, r3, r4);		LK(r2, r1, r3, r0, r4, 1);
-	S1(r2, r1, r3, r0, r4);		LK(r4, r3, r0, r2, r1, 2);
-	S2(r4, r3, r0, r2, r1);		LK(r1, r3, r4, r2, r0, 3);
-	S3(r1, r3, r4, r2, r0);		LK(r2, r0, r3, r1, r4, 4);
-	S4(r2, r0, r3, r1, r4);		LK(r0, r3, r1, r4, r2, 5);
-	S5(r0, r3, r1, r4, r2);		LK(r2, r0, r3, r4, r1, 6);
-	S6(r2, r0, r3, r4, r1);		LK(r3, r1, r0, r4, r2, 7);
-	S7(r3, r1, r0, r4, r2);		LK(r2, r0, r4, r3, r1, 8);
-	S0(r2, r0, r4, r3, r1);		LK(r4, r0, r3, r2, r1, 9);
-	S1(r4, r0, r3, r2, r1);		LK(r1, r3, r2, r4, r0, 10);
-	S2(r1, r3, r2, r4, r0);		LK(r0, r3, r1, r4, r2, 11);
-	S3(r0, r3, r1, r4, r2);		LK(r4, r2, r3, r0, r1, 12);
-	S4(r4, r2, r3, r0, r1);		LK(r2, r3, r0, r1, r4, 13);
-	S5(r2, r3, r0, r1, r4);		LK(r4, r2, r3, r1, r0, 14);
-	S6(r4, r2, r3, r1, r0);		LK(r3, r0, r2, r1, r4, 15);
-	S7(r3, r0, r2, r1, r4);		LK(r4, r2, r1, r3, r0, 16);
-	S0(r4, r2, r1, r3, r0);		LK(r1, r2, r3, r4, r0, 17);
-	S1(r1, r2, r3, r4, r0);		LK(r0, r3, r4, r1, r2, 18);
-	S2(r0, r3, r4, r1, r2);		LK(r2, r3, r0, r1, r4, 19);
-	S3(r2, r3, r0, r1, r4);		LK(r1, r4, r3, r2, r0, 20);
-	S4(r1, r4, r3, r2, r0);		LK(r4, r3, r2, r0, r1, 21);
-	S5(r4, r3, r2, r0, r1);		LK(r1, r4, r3, r0, r2, 22);
-	S6(r1, r4, r3, r0, r2);		LK(r3, r2, r4, r0, r1, 23);
-	S7(r3, r2, r4, r0, r1);		LK(r1, r4, r0, r3, r2, 24);
-	S0(r1, r4, r0, r3, r2);		LK(r0, r4, r3, r1, r2, 25);
-	S1(r0, r4, r3, r1, r2);		LK(r2, r3, r1, r0, r4, 26);
-	S2(r2, r3, r1, r0, r4);		LK(r4, r3, r2, r0, r1, 27);
-	S3(r4, r3, r2, r0, r1);		LK(r0, r1, r3, r4, r2, 28);
-	S4(r0, r1, r3, r4, r2);		LK(r1, r3, r4, r2, r0, 29);
-	S5(r1, r3, r4, r2, r0);		LK(r0, r1, r3, r2, r4, 30);
-	S6(r0, r1, r3, r2, r4);		LK(r3, r4, r1, r2, r0, 31);
-	S7(r3, r4, r1, r2, r0);
-	K(r0, r1, r2, r3, 32);
+	K( d0, d1, d2, d3, 0 );					S0( d0, d1, d2, d3, d4 );
+	LK( d2, d1, d3, d0, d4, 1 );		S1( d2, d1, d3, d0, d4 );
+	LK( d4, d3, d0, d2, d1, 2 );		S2( d4, d3, d0, d2, d1 );
+	LK( d1, d3, d4, d2, d0, 3 );		S3( d1, d3, d4, d2, d0 );
+	LK( d2, d0, d3, d1, d4, 4 );		S4( d2, d0, d3, d1, d4 );
+	LK( d0, d3, d1, d4, d2, 5 );		S5( d0, d3, d1, d4, d2 );
+	LK( d2, d0, d3, d4, d1, 6 );		S6( d2, d0, d3, d4, d1 );
+	LK( d3, d1, d0, d4, d2, 7 );		S7( d3, d1, d0, d4, d2 );
 
-	output_data[ 0 ] = r0;
-	output_data[ 1 ] = r1;
-	output_data[ 2 ] = r2;
-	output_data[ 3 ] = r3;
+	LK( d2, d0, d4, d3, d1, 8 );		S0( d2, d0, d4, d3, d1 );
+	LK( d4, d0, d3, d2, d1, 9 );		S1( d4, d0, d3, d2, d1 );
+	LK( d1, d3, d2, d4, d0, 10 );		S2( d1, d3, d2, d4, d0 );
+	LK( d0, d3, d1, d4, d2, 11 );		S3( d0, d3, d1, d4, d2 );
+	LK( d4, d2, d3, d0, d1, 12 );		S4( d4, d2, d3, d0, d1 );
+	LK( d2, d3, d0, d1, d4, 13 );		S5( d2, d3, d0, d1, d4 );
+	LK( d4, d2, d3, d1, d0, 14 );		S6( d4, d2, d3, d1, d0 );
+	LK( d3, d0, d2, d1, d4, 15 );		S7( d3, d0, d2, d1, d4 );
+
+	LK( d4, d2, d1, d3, d0, 16 );		S0( d4, d2, d1, d3, d0 );
+	LK( d1, d2, d3, d4, d0, 17 );		S1( d1, d2, d3, d4, d0 );
+	LK( d0, d3, d4, d1, d2, 18 );		S2( d0, d3, d4, d1, d2 );
+	LK( d2, d3, d0, d1, d4, 19 );		S3( d2, d3, d0, d1, d4 );
+	LK( d1, d4, d3, d2, d0, 20 );		S4( d1, d4, d3, d2, d0 );
+	LK( d4, d3, d2, d0, d1, 21 );		S5( d4, d3, d2, d0, d1 );
+	LK( d1, d4, d3, d0, d2, 22 );		S6( d1, d4, d3, d0, d2 );
+	LK( d3, d2, d4, d0, d1, 23 );		S7( d3, d2, d4, d0, d1 );
+
+	LK( d1, d4, d0, d3, d2, 24 );		S0( d1, d4, d0, d3, d2 );
+	LK( d0, d4, d3, d1, d2, 25 );		S1( d0, d4, d3, d1, d2 );
+	LK( d2, d3, d1, d0, d4, 26 );		S2( d2, d3, d1, d0, d4 );
+	LK( d4, d3, d2, d0, d1, 27 );		S3( d4, d3, d2, d0, d1 );
+	LK( d0, d1, d3, d4, d2, 28 );		S4( d0, d1, d3, d4, d2 );
+	LK( d1, d3, d4, d2, d0, 29 );		S5( d1, d3, d4, d2, d0 );
+	LK( d0, d1, d3, d2, d4, 30 );		S6( d0, d1, d3, d2, d4 );
+	LK( d3, d4, d1, d2, d0, 31 );		S7( d3, d4, d1, d2, d0 );
+
+	K( d0, d1, d2, d3, 32 );
+
+	output[ 0 ] = d0;
+	output[ 1 ] = d1;
+	output[ 2 ] = d2;
+	output[ 3 ] = d3;
 
 	return ( 1 );
 }
@@ -526,56 +531,60 @@ int libluksde_serpent_decrypt_block(
      libluksde_serpent_context_t *context,
      const uint8_t *input_data,
      uint8_t *output_data,
-     libcerror_error_t **error )
+     libcerror_error_t **error  )
 {
 	uint32_t *k 					= context->key;
-	uint32_t	r0, r1, r2, r3, r4;
+	uint32_t *input 			= ( uint32_t * ) input_data;
+	uint32_t *output 			= ( uint32_t * ) output_data;
+	uint32_t	d0, d1, d2, d3, d4;
 
-	r0 = input_data[ 0 ];
-	r1 = input_data[ 1 ];
-	r2 = input_data[ 2 ];
-	r3 = input_data[ 3 ];
+	d0 = input[ 0 ];
+	d1 = input[ 1 ];
+	d2 = input[ 2 ];
+	d3 = input[ 3 ];
 
-	K(r0, r1, r2, r3, 32);
-	SI7(r0, r1, r2, r3, r4);	KL(r1, r3, r0, r4, r2, 31);
-	SI6(r1, r3, r0, r4, r2);	KL(r0, r2, r4, r1, r3, 30);
-	SI5(r0, r2, r4, r1, r3);	KL(r2, r3, r0, r4, r1, 29);
-	SI4(r2, r3, r0, r4, r1);	KL(r2, r0, r1, r4, r3, 28);
-	SI3(r2, r0, r1, r4, r3);	KL(r1, r2, r3, r4, r0, 27);
-	SI2(r1, r2, r3, r4, r0);	KL(r2, r0, r4, r3, r1, 26);
-	SI1(r2, r0, r4, r3, r1);	KL(r1, r0, r4, r3, r2, 25);
-	SI0(r1, r0, r4, r3, r2);	KL(r4, r2, r0, r1, r3, 24);
-	SI7(r4, r2, r0, r1, r3);	KL(r2, r1, r4, r3, r0, 23);
-	SI6(r2, r1, r4, r3, r0);	KL(r4, r0, r3, r2, r1, 22);
-	SI5(r4, r0, r3, r2, r1);	KL(r0, r1, r4, r3, r2, 21);
-	SI4(r0, r1, r4, r3, r2);	KL(r0, r4, r2, r3, r1, 20);
-	SI3(r0, r4, r2, r3, r1);	KL(r2, r0, r1, r3, r4, 19);
-	SI2(r2, r0, r1, r3, r4);	KL(r0, r4, r3, r1, r2, 18);
-	SI1(r0, r4, r3, r1, r2);	KL(r2, r4, r3, r1, r0, 17);
-	SI0(r2, r4, r3, r1, r0);	KL(r3, r0, r4, r2, r1, 16);
-	SI7(r3, r0, r4, r2, r1);	KL(r0, r2, r3, r1, r4, 15);
-	SI6(r0, r2, r3, r1, r4);	KL(r3, r4, r1, r0, r2, 14);
-	SI5(r3, r4, r1, r0, r2);	KL(r4, r2, r3, r1, r0, 13);
-	SI4(r4, r2, r3, r1, r0);	KL(r4, r3, r0, r1, r2, 12);
-	SI3(r4, r3, r0, r1, r2);	KL(r0, r4, r2, r1, r3, 11);
-	SI2(r0, r4, r2, r1, r3);	KL(r4, r3, r1, r2, r0, 10);
-	SI1(r4, r3, r1, r2, r0);	KL(r0, r3, r1, r2, r4, 9);
-	SI0(r0, r3, r1, r2, r4);	KL(r1, r4, r3, r0, r2, 8);
-	SI7(r1, r4, r3, r0, r2);	KL(r4, r0, r1, r2, r3, 7);
-	SI6(r4, r0, r1, r2, r3);	KL(r1, r3, r2, r4, r0, 6);
-	SI5(r1, r3, r2, r4, r0);	KL(r3, r0, r1, r2, r4, 5);
-	SI4(r3, r0, r1, r2, r4);	KL(r3, r1, r4, r2, r0, 4);
-	SI3(r3, r1, r4, r2, r0);	KL(r4, r3, r0, r2, r1, 3);
-	SI2(r4, r3, r0, r2, r1);	KL(r3, r1, r2, r0, r4, 2);
-	SI1(r3, r1, r2, r0, r4);	KL(r4, r1, r2, r0, r3, 1);
-	SI0(r4, r1, r2, r0, r3);
-	K(r2, r3, r1, r4, 0);
+	K( d0, d1, d2, d3, 32 );				SI7( d0, d1, d2, d3, d4 );
+	KL( d1, d3, d0, d4, d2, 31 );		SI6( d1, d3, d0, d4, d2 );
+	KL( d0, d2, d4, d1, d3, 30 );		SI5( d0, d2, d4, d1, d3 );
+	KL( d2, d3, d0, d4, d1, 29 );		SI4( d2, d3, d0, d4, d1 );
+	KL( d2, d0, d1, d4, d3, 28 );		SI3( d2, d0, d1, d4, d3 );
+	KL( d1, d2, d3, d4, d0, 27 );		SI2( d1, d2, d3, d4, d0 );
+	KL( d2, d0, d4, d3, d1, 26 );		SI1( d2, d0, d4, d3, d1 );
+	KL( d1, d0, d4, d3, d2, 25 );		SI0( d1, d0, d4, d3, d2 );
 
-	output_data[ 0 ] = r2;
-	output_data[ 1 ] = r3;
-	output_data[ 2 ] = r1;
-	output_data[ 3 ] = r4;
+	KL( d4, d2, d0, d1, d3, 24 );		SI7( d4, d2, d0, d1, d3 );
+	KL( d2, d1, d4, d3, d0, 23 );		SI6( d2, d1, d4, d3, d0 );
+	KL( d4, d0, d3, d2, d1, 22 );		SI5( d4, d0, d3, d2, d1 );
+	KL( d0, d1, d4, d3, d2, 21 );		SI4( d0, d1, d4, d3, d2 );
+	KL( d0, d4, d2, d3, d1, 20 );		SI3( d0, d4, d2, d3, d1 );
+	KL( d2, d0, d1, d3, d4, 19 );		SI2( d2, d0, d1, d3, d4 );
+	KL( d0, d4, d3, d1, d2, 18 );		SI1( d0, d4, d3, d1, d2 );
+	KL( d2, d4, d3, d1, d0, 17 );		SI0( d2, d4, d3, d1, d0 );
+
+	KL( d3, d0, d4, d2, d1, 16 );		SI7( d3, d0, d4, d2, d1 );
+	KL( d0, d2, d3, d1, d4, 15 );		SI6( d0, d2, d3, d1, d4 );
+	KL( d3, d4, d1, d0, d2, 14 );		SI5( d3, d4, d1, d0, d2 );
+	KL( d4, d2, d3, d1, d0, 13 );		SI4( d4, d2, d3, d1, d0 );
+	KL( d4, d3, d0, d1, d2, 12 );		SI3( d4, d3, d0, d1, d2 );
+	KL( d0, d4, d2, d1, d3, 11 );		SI2( d0, d4, d2, d1, d3 );
+	KL( d4, d3, d1, d2, d0, 10 );		SI1( d4, d3, d1, d2, d0 );
+	KL( d0, d3, d1, d2, d4, 9 );		SI0( d0, d3, d1, d2, d4 );
+
+	KL( d1, d4, d3, d0, d2, 8 );		SI7( d1, d4, d3, d0, d2 );
+	KL( d4, d0, d1, d2, d3, 7 );		SI6( d4, d0, d1, d2, d3 );
+	KL( d1, d3, d2, d4, d0, 6 );		SI5( d1, d3, d2, d4, d0 );
+	KL( d3, d0, d1, d2, d4, 5 );		SI4( d3, d0, d1, d2, d4 );
+	KL( d3, d1, d4, d2, d0, 4 );		SI3( d3, d1, d4, d2, d0 );
+	KL( d4, d3, d0, d2, d1, 3 );		SI2( d4, d3, d0, d2, d1 );
+	KL( d3, d1, d2, d0, d4, 2 );		SI1( d3, d1, d2, d0, d4 );
+	KL( d4, d1, d2, d0, d3, 1 );		SI0( d4, d1, d2, d0, d3 );
+
+	K( d2, d3, d1, d4, 0 );
+
+	output[ 0 ] = d2;
+	output[ 1 ] = d3;
+	output[ 2 ] = d1;
+	output[ 3 ] = d4;
 
 	return ( 1 );
 }
-
